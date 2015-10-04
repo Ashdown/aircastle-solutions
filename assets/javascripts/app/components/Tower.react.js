3ee59f081574028297bcc6cc27679e3da0d04801
Rory Devane <rory.devane@gmail.com>
2015-10-04 18:01:26 +0100

    made all gradient ids unique to prevent clashing

--- assets/javascripts/app/components/Tower.react.js
+++ assets/javascripts/app/components/Tower.react.js
@@ -5,7 +5,7 @@
             return(
                 <svg className="tower">
                     <defs>
-                        <linearGradient id="Gradient_1" gradientUnits="userSpaceOnUse" x1="275.85" y1="237.575" x2="101.45" y2="62.124999999999986" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_1" gradientUnits="userSpaceOnUse" x1="275.85" y1="237.575" x2="101.45" y2="62.124999999999986" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#846F54"}}/>
 
                             <stop  offset="20.784313725490197%" style={{stopColor:"#9D8364"}}/>
@@ -17,7 +17,7 @@
                             <stop  offset="100%" style={{stopColor:"#836F54"}}/>
                         </linearGradient>
 
-                        <linearGradient id="Gradient_2" gradientUnits="userSpaceOnUse" x1="128.52499999999998" y1="254.875" x2="315.875" y2="67.52499999999998" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_2" gradientUnits="userSpaceOnUse" x1="128.52499999999998" y1="254.875" x2="315.875" y2="67.52499999999998" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#846F54"}}/>
 
                             <stop  offset="20.784313725490197%" style={{stopColor:"#9D8364"}}/>
@@ -29,7 +29,7 @@
                             <stop  offset="100%" style={{stopColor:"#836F54"}}/>
                         </linearGradient>
 
-                        <linearGradient id="Gradient_3" gradientUnits="userSpaceOnUse" x1="195.55" y1="94.7" x2="203.55" y2="94.7" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_3" gradientUnits="userSpaceOnUse" x1="195.55" y1="94.7" x2="203.55" y2="94.7" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#A8884D"}}/>
 
                             <stop  offset="20.784313725490197%" style={{stopColor:"#FDEE8D"}}/>
@@ -39,7 +39,7 @@
                             <stop  offset="100%" style={{stopColor:"#66512E"}}/>
                         </linearGradient>
 
-                        <radialGradient id="Gradient_4" gradientUnits="userSpaceOnUse" cx="0" cy="0" r="11.574999999999989" fx="0" fy="0" gradientTransform="matrix( 1.000000000000001, 0, 0, 1.000000000000001, 196.6,9.7) " spreadMethod="pad">
+                        <radialGradient id="tower_gradient_4" gradientUnits="userSpaceOnUse" cx="0" cy="0" r="11.574999999999989" fx="0" fy="0" gradientTransform="matrix( 1.000000000000001, 0, 0, 1.000000000000001, 196.6,9.7) " spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#FDEE8D"}}/>
 
                             <stop  offset="50.19607843137255%" style={{stopColor:"#AA894E"}}/>
@@ -47,7 +47,7 @@
                             <stop  offset="100%" style={{stopColor:"#66512E"}}/>
                         </radialGradient>
 
-                        <linearGradient id="Gradient_5" gradientUnits="userSpaceOnUse" x1="204.625" y1="55.9375" x2="265.375" y2="39.662499999999994" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_5" gradientUnits="userSpaceOnUse" x1="204.625" y1="55.9375" x2="265.375" y2="39.662499999999994" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#666666"}}/>
 
                             <stop  offset="38.03921568627451%" style={{stopColor:"#CCCCCC"}}/>
@@ -57,19 +57,19 @@
                             <stop  offset="100%" style={{stopColor:"#CCCCCC"}}/>
                         </linearGradient>
 
-                        <linearGradient id="Gradient_6" gradientUnits="userSpaceOnUse" x1="252.86249999999998" y1="72.6125" x2="266.4375" y2="68.9875" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_6" gradientUnits="userSpaceOnUse" x1="252.86249999999998" y1="72.6125" x2="266.4375" y2="68.9875" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#999999"}}/>
 
                             <stop  offset="100%" style={{stopColor:"#666666"}}/>
                         </linearGradient>
 
-                        <linearGradient id="Gradient_7" gradientUnits="userSpaceOnUse" x1="269.75" y1="107.75" x2="294.04999999999995" y2="101.25" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_7" gradientUnits="userSpaceOnUse" x1="269.75" y1="107.75" x2="294.04999999999995" y2="101.25" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#999999"}}/>
 
                             <stop  offset="100%" style={{stopColor:"#666666"}}/>
                         </linearGradient>
 
-                        <linearGradient id="Gradient_8" gradientUnits="userSpaceOnUse" x1="257.075" y1="93.71249999999999" x2="293.72499999999997" y2="83.8875" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_8" gradientUnits="userSpaceOnUse" x1="257.075" y1="93.71249999999999" x2="293.72499999999997" y2="83.8875" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#666666"}}/>
 
                             <stop  offset="38.03921568627451%" style={{stopColor:"#CCCCCC"}}/>
@@ -79,7 +79,7 @@
                             <stop  offset="100%" style={{stopColor:"#CCCCCC"}}/>
                         </linearGradient>
 
-                        <linearGradient id="Gradient_9" gradientUnits="userSpaceOnUse" x1="274.9375" y1="136.075" x2="357.5625" y2="113.925" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_9" gradientUnits="userSpaceOnUse" x1="274.9375" y1="136.075" x2="357.5625" y2="113.925" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#666666"}}/>
 
                             <stop  offset="30.58823529411765%" style={{stopColor:"#CCCCCC"}}/>
@@ -89,7 +89,7 @@
                             <stop  offset="100%" style={{stopColor:"#CCCCCC"}}/>
                         </linearGradient>
 
-                        <linearGradient id="Gradient_10" gradientUnits="userSpaceOnUse" x1="200.04999999999998" y1="280.1" x2="225.85" y2="280.1" spreadMethod="pad">
+                        <linearGradient id="tower_gradient_10" gradientUnits="userSpaceOnUse" x1="200.04999999999998" y1="280.1" x2="225.85" y2="280.1" spreadMethod="pad">
                             <stop  offset="0%" style={{stopColor:"#000000", stopOpacity:"20%"}}/>
 
                             <stop  offset="100%" style={{stopColor:"#000000", stopOpacity:"0%"}}/>
@@ -97,21 +97,21 @@
                     </defs>
 
                     <g>
-                        <path fill="url(#Gradient_1)" stroke="none" d="
+                        <path fill="url(#tower_gradient_1)" stroke="none" d="
                             M 195.55 121.5
                             L 175.05 142 175.05 467 200.05 467 200.05 117 195.55 121.5 Z"/>
 
-                        <path fill="url(#Gradient_2)" stroke="none" d="
+                        <path fill="url(#tower_gradient_2)" stroke="none" d="
                             M 225.05 142
                             L 203.55 120.5 200.05 117 200.05 467 225.05 467 225.05 142 Z"/>
 
-                        <path fill="url(#Gradient_3)" stroke="none" d="
+                        <path fill="url(#tower_gradient_3)" stroke="none" d="
                             M 203.55 29.6
                             L 203.55 19
                             Q 201.8 17.9 199.55 17.9 197.3 17.9 195.55 19
                             L 195.55 121.5 200.05 117 203.55 120.5 203.55 66.2 203.55 29.6 Z"/>
 
-                        <path fill="url(#Gradient_4)" stroke="none" d="
+                        <path fill="url(#tower_gradient_4)" stroke="none" d="
                             M 204.85 17.85
                             Q 207.05 15.6 207.05 12.5 207.05 9.45 204.85 7.2 202.65 5 199.55 5 196.45 5 194.25 7.2 192.05 9.45 192.05 12.5 192.05 15.6 194.25 17.85 194.85 18.45 195.55 18.9
                             L 195.55 19
@@ -119,7 +119,7 @@
                             L 203.55 18.9
                             Q 204.25 18.45 204.85 17.85 Z"/>
 
-                        <path fill="url(#Gradient_5)" stroke="none" d="
+                        <path fill="url(#tower_gradient_5)" stroke="none" d="
                             M 252.65 62.65
                             Q 264.75 62.75 266.45 65.2
                             L 260.25 43.35
@@ -127,30 +127,30 @@
                             L 203.55 66.2
                             Q 236.9 62.5 252.65 62.65 Z"/>
 
-                        <path fill="url(#Gradient_6)" stroke="none" d="
+                        <path fill="url(#tower_gradient_6)" stroke="none" d="
                             M 256.45 78.95
                             Q 256.35 74.3 263.05 70.1 267.7 67 266.45 65.2 264.75 62.75 252.65 62.65
                             L 256.45 78.95 Z"/>
 
-                        <path fill="url(#Gradient_7)" stroke="none" d="
+                        <path fill="url(#tower_gradient_7)" stroke="none" d="
                             M 275.55 95.75
                             Q 272.15 94.15 269.35 92.6
                             L 273.5 116.45
                             Q 273.25 111.7 281.7 110.35 282.15 110.3 282.6 110.25 284.85 109.95 287.7 109.85 294.7 110 294.5 107.9 294.5 107.7 294.4 107.5 293 104.05 275.55 95.75 Z"/>
 
-                        <path fill="url(#Gradient_8)" stroke="none" d="
+                        <path fill="url(#tower_gradient_8)" stroke="none" d="
                             M 269.35 92.6
                             Q 272.15 94.15 275.55 95.75 293 104.05 294.4 107.5
                             L 287.5 81.85
                             Q 286.95 75.3 263.05 70.1 256.35 74.3 256.45 78.95 256.55 85.35 269.35 92.6 Z"/>
 
-                        <path fill="url(#Gradient_9)" stroke="none" d="
+                        <path fill="url(#tower_gradient_9)" stroke="none" d="
                             M 336.45 138.95
                             Q 351.75 137.5 359.05 135.4 353.45 128.35 353.65 115.65 312.1 129.85 281.7 110.35 273.25 111.7 273.5 116.45 273.65 120.65 280.65 127.45 286.6 133.1 292.85 135.25 298.55 137.2 309.85 138.8 321.1 140.4 336.45 138.95 Z"/>
                     </g>
 
                     <g>
-                        <path fill="url(#Gradient_10)" stroke="none" d="
+                        <path fill="url(#tower_gradient_10)" stroke="none" d="
                             M 225.05 142
                             L 200.05 117 200.05 467 225.05 467 225.05 142 Z"/>
                     </g>
