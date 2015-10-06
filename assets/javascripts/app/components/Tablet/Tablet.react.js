54bb60ce5c192bf2e3a734b323404292d42856e7
Rory Devane <rory.devane@gmail.com>
2015-10-06 12:48:45 +0100

    made common shine css class

--- assets/javascripts/app/components/Tablet/Tablet.react.js
+++ assets/javascripts/app/components/Tablet/Tablet.react.js
@@ -7,10 +7,10 @@
             return(
                 <div className='tablet'>
                     <div className='gold-border'>
-                        <div className='gold-border-shine'></div>
+                        <div className='shine gold-border-shine'></div>
                         <div className='black-border'>
                             <div className='face'>
-                                <div className="face-shine"></div>
+                                <div className="shine face-shine"></div>
                                 <App.Components.FaceContent/>
                             </div>
                         </div>
