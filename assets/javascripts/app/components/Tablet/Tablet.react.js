c1dc93ee62d07f6d1264308fd6320685a518f8e3
Rory Devane <rory.devane@gmail.com>
2015-10-06 12:42:45 +0100

    added gold border shine and face shine

--- assets/javascripts/app/components/Tablet/Tablet.react.js
+++ assets/javascripts/app/components/Tablet/Tablet.react.js
@@ -7,33 +7,36 @@
             return(
                 <div className='tablet'>
                     <div className='gold-border'>
-                        <div className='gold-shine'></div>
+                        <div className='gold-border-shine'></div>
                         <div className='black-border'>
                             <div className='face'>
-                                <App.Components.Header/>
-                                <p>Software and web development</p>
-                                <h2>Specialising in</h2>
-                                <ul>
-                                    <li>
-                                        <h3><App.Components.Technology text="JavaScript"/></h3>
-                                        <ul>
-                                            <li><App.Components.Technology text="React"/></li>
-                                            <li><App.Components.Technology text="AngularJS"/></li>
-                                            <li><App.Components.Technology text="Ember.js"/></li>
-                                            <li><App.Components.Technology text="jQuery"/></li>
-                                        </ul>
-                                    </li>
-                                    <li><App.Components.Technology text="Raphaël"/></li>
-                                    <li><App.Components.Technology text="CSS"/></li>
-                                    <li><App.Components.Technology text="Ruby Sinatra"/></li>
-                                    <li><App.Components.Technology text="Heroku"/></li>
-                                </ul>
-                                <h2> Links</h2>
-                                <ul>
-                                    <li>
-                                        <a href="https://github.com/"> GitHub</a>
-                                    </li>
-                                </ul>
+                                <div className="face-shine"></div>
+                                <div className="face-content">
+                                    <App.Components.Header/>
+                                    <p>Software and web development</p>
+                                    <h2>Specialising in</h2>
+                                    <ul>
+                                        <li>
+                                            <h3><App.Components.Technology text="JavaScript"/></h3>
+                                            <ul>
+                                                <li><App.Components.Technology text="React"/></li>
+                                                <li><App.Components.Technology text="AngularJS"/></li>
+                                                <li><App.Components.Technology text="Ember.js"/></li>
+                                                <li><App.Components.Technology text="jQuery"/></li>
+                                            </ul>
+                                        </li>
+                                        <li><App.Components.Technology text="Raphaël"/></li>
+                                        <li><App.Components.Technology text="CSS"/></li>
+                                        <li><App.Components.Technology text="Ruby Sinatra"/></li>
+                                        <li><App.Components.Technology text="Heroku"/></li>
+                                    </ul>
+                                    <h2> Links</h2>
+                                    <ul>
+                                        <li>
+                                            <a href="https://github.com/"> GitHub</a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
