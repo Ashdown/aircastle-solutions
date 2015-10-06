67e58daf942fdda85eaee37e27bf0af8be261a85
Rory Devane <rory.devane@gmail.com>
2015-10-06 14:57:38 +0100

    added fluid grid to face content

--- assets/javascripts/app/components/Tablet/FaceContent.react.js
+++ assets/javascripts/app/components/Tablet/FaceContent.react.js
@@ -1,28 +1,49 @@
 (function(App, React){
 
     App.Components.FaceContent = React.createClass({
-        render: function(){
+        render: function() {
             return(
                 <div className="face-content">
                     <App.Components.Header/>
-                    <p><App.Components.Text content="Software and web development"/></p>
-                    <h2><App.Components.Text content="Specialising in"/></h2>
+                    <h2>
+                        <App.Components.Text content="Software Development"/>
+                    </h2>
                     <ul>
                         <li>
-                            <h3><App.Components.Text content="JavaScript"/></h3>
+                            <h3>
+                                <App.Components.Text content="JavaScript"/>
+                            </h3>
                             <ul>
-                                <li><App.Components.Text content="React"/></li>
-                                <li><App.Components.Text content="AngularJS"/></li>
-                                <li><App.Components.Text content="Ember.js"/></li>
-                                <li><App.Components.Text content="jQuery"/></li>
+                                <li>
+                                    <App.Components.Text content="React"/>
+                                </li>
+                                <li>
+                                    <App.Components.Text content="AngularJS"/>
+                                </li>
+                                <li>
+                                    <App.Components.Text content="Ember.js"/>
+                                </li>
+                                <li>
+                                    <App.Components.Text content="jQuery"/>
+                                </li>
+                                <li>
+                                    <App.Components.Text content="Raphaël"/>
+                                </li>
                             </ul>
                         </li>
-                        <li><App.Components.Text content="Raphaël"/></li>
-                        <li><App.Components.Text content="CSS"/></li>
-                        <li><App.Components.Text content="Ruby Sinatra"/></li>
-                        <li><App.Components.Text content="Heroku"/></li>
+                        <li>
+                            <App.Components.Text content="CSS"/>
+                        </li>
+                        <li>
+                            <App.Components.Text content="Ruby Sinatra"/>
+                        </li>
+                        <li>
+                            <App.Components.Text content="Heroku"/>
+                        </li>
                     </ul>
-                    <h2><App.Components.Text content="Links"/></h2>
+                    <h2>
+                        <App.Components.Text content="Links"/>
+                    </h2>
                     <ul>
                         <li>
                             <a href="https://github.com/"> GitHub</a>
@@ -31,6 +52,7 @@
                 </div>
                 );
         }
+
     });
 
 })(App, React);
