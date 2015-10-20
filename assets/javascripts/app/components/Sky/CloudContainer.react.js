40ff900ad9b207b4ff78c3edd1737548955fdf02
Rory Devane <rory.devane@gmail.com>
2015-10-20 16:58:21 +0100

    tablet shines now move as you scroll

--- assets/javascripts/app/components/Sky/CloudContainer.react.js
+++ assets/javascripts/app/components/Sky/CloudContainer.react.js
@@ -1,7 +1,7 @@
 (function(App, React){
     App.Components.CloudContainer = React.createClass({
 
-        handleScroll: function(){
+        handleScroll: function(event){
             var newOffset = event.srcElement.body.scrollTop * App.Constants.SkyConstants.SKY_OFFSET_SPEED;
 
             if(newOffset >= 0 && newOffset <= App.Constants.SkyConstants.SKY_OFFSET_MAX_LIMIT) {
--- assets/javascripts/app/components/Tablet/Tablet.react.js
+++ assets/javascripts/app/components/Tablet/Tablet.react.js
@@ -2,15 +2,33 @@
 
     App.Components.Tablet = React.createClass({
 
+        handleScroll: function(event) {
+
+            var scrollTop = event.srcElement.body.scrollTop;
+
+            if(scrollTop > 300) {
+                this.setState({
+                    shineOffset: scrollTop - 300
+                });
+            }
+            
+        },
+
+        getInitialState: function() {
+          return{
+              shineOffset: 0
+          };
+        },
+
         render: function () {
 
             return(
                 <div className='tablet'>
                     <div className='gold-border'>
-                        <div className='shine gold-border-shine'></div>
+                        <div className='shine gold-border-shine' style={{top: this.state.shineOffset + 'px'}}></div>
                         <div className='black-border'>
                             <div className='face'>
-                                <div className="shine face-shine"></div>
+                                <div className="shine face-shine" style={{top: this.state.shineOffset + 'px'}}></div>
                                 <App.Components.FaceContent/>
                             </div>
                         </div>
@@ -20,8 +38,16 @@
                     <div className="background background-bottom"></div>
                     <div className="background background-fade"></div>
                 </div>
-                );
+            );
 
+        },
+
+        componentDidMount: function() {
+            window.addEventListener('scroll', this.handleScroll);
+        },
+
+        componentWillUnmount: function() {
+            window.removeEventListener('scroll', this.handleScroll);
         }
     });
 
